e6d3c3094f68597198f87bab4e8e71a4b15f051b
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-06-11 22:24:14 +0800

    delete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(email: "thoriq@gmail.com", password: "password", password_confirmation: "password")
-User.create!(email: "ali@gmail.com", password: "password", password_confirmation: "password")
-User.create!(email: "said@gmail.com", password: "password", password_confirmation: "password")
+# User.create!(email: "thoriq@gmail.com", password: "password", password_confirmation: "password")
+# User.create!(email: "ali@gmail.com", password: "password", password_confirmation: "password")
+# User.create!(email: "said@gmail.com", password: "password", password_confirmation: "password")
 
-Room.create!(name: "Ruby On Rails");
-Room.create!(name: "Javascript");
-Room.create!(name: "Random");
+# Room.create!(name: "Ruby On Rails");
+# Room.create!(name: "Javascript");
+# Room.create!(name: "Random");
